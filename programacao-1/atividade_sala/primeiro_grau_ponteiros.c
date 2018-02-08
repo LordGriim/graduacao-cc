656e215968aa5186a46ecfbb74e9b5c238e17d1a
Jackson Barbosa <jacksonbarbosa321@gmail.com>
2018-02-08 20:29:30 -0300

    Ajuste do input

--- programacao-1/atividade_sala/primeiro_grau_ponteiros.c
+++ programacao-1/atividade_sala/primeiro_grau_ponteiros.c
@@ -12,11 +12,8 @@ int main(void) {
 	primeiro_grau(0, &fx, &gx);
 	printf("f(%d) = %d\ng(%d) = %d\n", 0, fx, 0, gx);
 	
-	primeiro_grau(1, &fx, &gx);
+	primeiro_grau(5, &fx, &gx);
 	printf("f(%d) = %d\ng(%d) = %d\n", 1, fx, 1, gx);
-
-	primeiro_grau(2, &fx, &gx);
-	printf("f(%d) = %d\ng(%d) = %d\n", 2, fx, 2, gx);
 	
 	return 0;
 }
